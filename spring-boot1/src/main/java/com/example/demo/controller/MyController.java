903ff349f8257f1c17ff8a1ff85056adf3dd854b
blueskyarea <blueskyareahm@gmail.com>
2021-09-19 09:49:45 +0900

    spring-boot1

--- spring-boot1/src/main/java/com/example/demo/controller/MyController.java
+++ spring-boot1/src/main/java/com/example/demo/controller/MyController.java
@@ -4,8 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloController {
-	@RequestMapping("/hello")
+public class MyController {
+	@RequestMapping("/")
 	public String index() {
 		return "hello";
 	}
