7082b87de388fbbb0b9eca9d3eef53ac7c203100
blueskyarea <blueskyareahm@gmail.com>
2021-12-23 23:05:41 +0900

    Added practice2

---
+++ spring-boot2/src/main/java/com/example/practice/controller/MyController.java
@@ -0,0 +1,19 @@
+package com.example.practice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController {
+	@GetMapping("/")
+	public String index() {
+		return "hello";
+	}
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return null;
+	}
+}
