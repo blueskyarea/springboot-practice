35ac5f59aae78dd2cc8f11d6ed2b71a68ee5ecfc
blueskyarea <blueskyareahm@gmail.com>
2022-02-12 22:48:00 +0900

    Fixfor Practice3.

---
+++ spring-boot3/src/main/java/com/example/demo/controller/MyController_sample.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MyController_sample {
+	@GetMapping("/")
+	public String index() {
+		return "myform";
+	}
+	
+	@PostMapping("/myresponse")
+	public String myresponse(@RequestParam String message, Model model) {
+		model.addAttribute("message", message);
+		return "myresponse";
+	}
+}
