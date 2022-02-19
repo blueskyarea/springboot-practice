6f73a77c279ef506d4e403aec4138e6a03f1d368
blueskyarea <blueskyareahm@gmail.com>
2022-02-19 22:45:45 +0900

    Fix for Practice5 and create Practice6.

--- spring-boot5/src/main/java/com/example/demo/controller/MyController.java
+++ spring-boot5/src/main/java/com/example/demo/controller/MyController.java
@@ -1,20 +1,8 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import com.example.demo.model.User;
 
 @Controller
 public class MyController {
-	@GetMapping("/form")
-	private String readForm(@ModelAttribute User user) {
-		return "form";
-	}
-	
-	@PostMapping("/form")
-	private String confirm(@ModelAttribute User user) {
-		return "confirm";
-	}
+	// TODO: Please implement here
 }
